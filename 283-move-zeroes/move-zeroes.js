29d09d8db4bd42a2d18c8e2c3ed1c80b65a0ca25
Rithik yadav <rithikyadav210@gmail.com>
2026-08-29 22:39:17 +0530

    Time: 4 ms (30.24%) | Memory: 60.2 MB (57.69%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    if(nums.length===1){
+        return[nums]
+    }
+    let j=0
+    for(let i = 0;i<nums.length;i++){
+        if(nums[i]!==0){
+            [nums[i],nums[j]]=[nums[j],nums[i]]
+            j++
+        }
+    }
+};
