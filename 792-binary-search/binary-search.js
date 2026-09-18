c853a39285f9795959e85c9343a0551770caaeb3
Rithik yadav <rithikyadav210@gmail.com>
2026-09-18 20:20:40 +0530

    Time: 0 ms (100.00%) | Memory: 56.5 MB (96.08%) - LeetSync

---
+++ 792-binary-search/binary-search.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+   let low=0
+   let high = nums.length-1;
+    while(low <= high){
+        const mid=Math.floor((low + high)/2);
+
+        if(nums[mid]===target){
+    return mid
+    }
+    else if(nums[mid]>target){
+    high= mid - 1
+    }else{
+    low=mid + 1
+    }
+    }
+   
+    return -1
+
+
+};
