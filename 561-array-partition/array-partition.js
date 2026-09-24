73b9a4dc46a45ad7e56d02f0f41a9907e438a710
Rithik yadav <rithikyadav210@gmail.com>
2026-09-24 19:48:19 +0530

    Time: 41 ms (44.22%) | Memory: 60.9 MB (36.96%) - LeetSync

--- 561-array-partition/array-partition.js
+++ 561-array-partition/array-partition.js
@@ -5,8 +5,8 @@
 var arrayPairSum = function(nums) {
     let total=0
     nums=nums.sort((a,b)=>a-b);
-    for(let i =0;i<nums.length;i+=2){
-        total+=nums[i]
+    for(let i =0;i<nums.length;i=i+2){
+        total=total+nums[i]
         
     }
     return total
