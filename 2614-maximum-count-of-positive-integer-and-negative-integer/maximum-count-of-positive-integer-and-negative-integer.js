b4c0c9310492b1a1f48deddfd582ce30514d76dd
Rithik yadav <rithikyadav210@gmail.com>
2026-09-26 21:14:50 +0530

    Time: 1 ms (40.00%) | Memory: 56.2 MB (39.20%) - LeetSync

---
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumCount = function(nums) {
+    let positive=0
+    let negative=0
+    
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]>0)
+        positive++
+        if(nums[i]<0){
+            negative++
+        }
+    }
+    return Math.max(positive,negative)
+    
+};
